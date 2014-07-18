78852fb33fa2a059b8f6a46228f3a62c537e03d0
nanawel <nanawel@gmail.com>
2014-07-19 01:23:19 +0200

    Removed User-agent header

--- 9boxwatcher-github/libs/Neufbox4.class.php
+++ 9boxwatcher-github/libs/Neufbox4.class.php
@@ -119,7 +119,7 @@ class Neufbox4 {
 						'password' => '',
 						'zsid' => $this->_sessionId,
 					),
-					array('User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0'),
+					null,
 					$this->_getSidCookie()
 				);
 			}
